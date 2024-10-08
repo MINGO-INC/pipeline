bd4b074f2a28f07295d5d169d1304b87d84e789e
MINGO-INC <143798588+MINGO-INC@users.noreply.github.com>
2024-10-08 11:35:10 +0200

    hej
    hej

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -2,7 +2,9 @@ package dat;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
+        Student student=new Student("ja",23,1.2);
+        System.out.println(student.getName());
 
     }
 }
