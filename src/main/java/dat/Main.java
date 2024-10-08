da3f24046be66079ab82b679575872ef21059509
MINGO-INC <143798588+MINGO-INC@users.noreply.github.com>
2024-10-08 12:04:42 +0200

    pushed javalin

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,10 +1,12 @@
 package dat;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Student student=new Student("ja",23,1.2);
-        System.out.println(student.getName());
+
+        Javalin app = Javalin.create().start(7070);
+
 
     }
 }
